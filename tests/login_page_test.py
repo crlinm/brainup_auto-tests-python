4c153bfe06010b329bc6d386a1d33c21a7926813
Alina M <crlin.m@gmail.com>
2023-12-14 02:14:05 +0600

    feat: add some allure annotations

--- tests/login_page_test.py
+++ tests/login_page_test.py
@@ -1,11 +1,14 @@
 import time
 
+import allure
+
 from pages.autorized_user_home_page import AuthorizedUserHomePage
 from pages.login_page import LoginPage
 from pages.speech_exercises_page import SpeechExercisesPage
 from test_data.links import MainPageLinks
 
 
+@allure.epic("Login Page")
 class TestLoginPage:
 
     def test_logining_user(self, driver, main_page_open):
@@ -14,6 +17,7 @@ class TestLoginPage:
         assert login_page_url == MainPageLinks.URL_GROUPS_PAGE, "The link leads to an incorrect page"
 
 
+@allure.epic("Exercises")
 class TestTask:
     def test_task(self, driver, main_page_open):
         page = LoginPage(driver)
